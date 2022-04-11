b18bc41339b2cfae6d4fcc27a835eb0822810acb
NotDarkytb <88250458+NotDarkytb@users.noreply.github.com>
2022-04-11 20:18:03 +0200

    Add files via upload

---
+++ cartel/config.lua
@@ -0,0 +1,21 @@
+Config             = {}
+
+Config.DrawDistance = 50
+Config.Size         = {x = 1.0, y = 1.0, z = 1.0}
+Config.Color        = {r = 255, g = 255, b = 255}
+Config.Type         = 20
+Config.EnableESXIdentity          = true 
+Config.EnableLicenses             = true
+
+Config.pos = {
+	coffre = {
+		position = {x = -1517.78, y = 132.77, z = 55.67}
+	},
+	garage = {
+		position = {x = -1546.91, y = 129.31, z = 56.78}
+	},
+
+	spawnvoiture = {
+		position = {x = -1556.36, y = 126.13, z = 56.81, h = 131.9}
+	},
+}
